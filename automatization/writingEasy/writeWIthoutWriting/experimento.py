d54cbb568b2c0494e91ff102d4ac2fbd5e6741c6
alt09 <andreslaratrevino@gmail.com>
2024-06-12 13:52:11 -0700

    orginize the folders

--- automatization/writingEasy/writeWIthoutWriting/experimento.py
+++ automatization/writingEasy/writeWIthoutWriting/experimento.py
@@ -3,8 +3,10 @@ import time
 import pyautogui as auto 
 time.sleep(5)
 
-with open('theText.txt',  'r') as theFile:
+with open('theText.txt','r') as theFile:
     auto.FAILSAFE = False
     for line in theFile:
         auto.typewrite(line)
         auto.press('auto') 
+
+
