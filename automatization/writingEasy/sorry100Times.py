b57160062248899f1b0275486e55611836ff0d14
alt09 <andreslaratrevino@gmail.com>
2024-06-13 00:25:34 -0700

    add comments to the  sorry 100 times

--- automatization/writingEasy/sorry100Times.py
+++ automatization/writingEasy/sorry100Times.py
@@ -1,3 +1,6 @@
+# This script prints "I LOVE BEN" followed by a number, 1000 times.
 
+# Loop through the numbers 0 to 999 (inclusive)
 for i in range(1000):
-    print('I LOVE BEN (' + str(i +1) + ')')
+    # Print the message "I LOVE BEN" followed by the current number (starting from 1)
+    print('I LOVE BEN (' + str(i + 1) + ')')
