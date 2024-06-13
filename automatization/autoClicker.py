366ac43daa1003dac1f0869640ea2b437f7d9145
alt09 <andreslaratrevino@gmail.com>
2024-06-13 00:25:20 -0700

    add comments to the auto clicker

--- automatization/autoClicker.py
+++ automatization/autoClicker.py
@@ -1,6 +1,14 @@
+# This script simulates 100 mouse clicks using the pyautogui library after a delay of 5 seconds.
+
 import time 
 import pyautogui as auto 
+
+# Pause the script for 5 seconds to allow the user to prepare (e.g., move the mouse to the desired location)
 time.sleep(5)
+
+# Disable the fail-safe feature of pyautogui, which stops the script if the mouse is moved to a corner of the screen
 auto.FAILSAFE = False
+
+# Simulate 100 mouse clicks
 for i in range(100):
     auto.click()
