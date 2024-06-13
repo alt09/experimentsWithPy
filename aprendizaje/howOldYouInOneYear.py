42795f15a028cd3ab89753544cc93f1a5d5e4bb4
alt09 <andreslaratrevino@gmail.com>
2024-06-13 00:24:52 -0700

    add coments to the how old you are in one year practice

--- aprendizaje/howOldYouInOneYear.py
+++ aprendizaje/howOldYouInOneYear.py
@@ -1,7 +1,22 @@
+# This script interacts with the user by asking for their name and age, 
+#then responds with a greeting and a comment about their age next year.
 
-print('what is your name?')
+# Print a prompt asking for the user's name
+print('What is your name?')
+
+# Read the user's input (their name) and store it in the variable 'myName'
 myName = input()
-print('hi ' + myName)
-print ('how old are you' )
+
+# Print a greeting message that includes the user's name
+print('Hi ' + myName)
+
+# Print a prompt asking for the user's age
+print('How old are you?')
+
+# Read the user's input (their age) and store it in the variable 'age'
 age = input()
-print('wow you are in one year'+ str(int(age)+1))
+
+# Calculate the user's age next year by converting the age to an integer, adding 1, 
+#and converting it back to a string
+# Print a message that comments on the user's age next year
+print('Wow, you will be ' + str(int(age) + 1) + ' in one year!')
